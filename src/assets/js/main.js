753ca0cee2f11b8a48c53c324ee6f6935ee5672d
lev <leff.luther98@yandex.ru>
2025-07-29 03:55:44 +0300

    refactoring

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,18 +1,16 @@
 import ready from './utils/documentReady';
+import custom from './components/custom';
 import homeSlider from "./components/homeSlider";
 import callbackModal from './components/callbackModal';
 import mmenu from './components/mmenu';
 import catalogSlider from './components/catalogSlider';
 import worthSlider from './components/worthSlider';
-import worthClick from './components/worthClick';
-import description from './components/description';
 
 ready(() => {
+    custom()
     mmenu()
     callbackModal()
     homeSlider()
     catalogSlider()
     worthSlider()
-    worthClick()
-    description()
 })
